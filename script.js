e89c9f0db7c6773bba348feb5336873741aaa430
DivyanshVinayak23 <65464884+DivyanshVinayak23@users.noreply.github.com>
2023-01-10 23:42:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+var cell1 = document.querySelector("#cell1");
+
+cell1.addEventListener("click",function(){
+    cell1.textContent = "X";
+})
+
+cell1.addEventListener("dblclick",function(){
+    cell1.textContent = "O";
+})
+
+var cell2 = document.querySelector("#cell2");
+
+cell2.addEventListener("click",function(){
+    cell2.textContent = "X";
+})
+
+cell2.addEventListener("dblclick",function(){
+    cell2.textContent = "O";
+})
+
+
+var cell3 = document.querySelector("#cell3");
+
+cell3.addEventListener("click",function(){
+    cell3.textContent = "X";
+})
+
+cell3.addEventListener("dblclick",function(){
+    cell3.textContent = "O";
+})
+
+
+var cell4 = document.querySelector("#cell4");
+
+cell4.addEventListener("click",function(){
+    cell4.textContent = "X";
+})
+
+cell4.addEventListener("dblclick",function(){
+    cell4.textContent = "O";
+})
+
+
+var cell5 = document.querySelector("#cell5");
+
+cell5.addEventListener("click",function(){
+    cell5.textContent = "X";
+})
+
+cell5.addEventListener("dblclick",function(){
+    cell5.textContent = "O";
+})
+
+
+var cell6 = document.querySelector("#cell6");
+
+cell6.addEventListener("click",function(){
+    cell6.textContent = "X";
+})
+
+cell6.addEventListener("dblclick",function(){
+    cell6.textContent = "O";
+})
+
+
+var cell7 = document.querySelector("#cell7");
+
+cell7.addEventListener("click",function(){
+    cell7.textContent = "X";
+})
+
+cell7.addEventListener("dblclick",function(){
+    cell7.textContent = "O";
+})
+
+
+var cell8 = document.querySelector("#cell8");
+
+cell8.addEventListener("click",function(){
+    cell8.textContent = "X";
+})
+
+cell8.addEventListener("dblclick",function(){
+    cell8.textContent = "O";
+})
+
+
+var cell9 = document.querySelector("#cell9");
+
+cell9.addEventListener("click",function(){
+    cell9.textContent = "X";
+})
+
+cell9.addEventListener("dblclick",function(){
+    cell9.textContent = "O";
+})
+
+
+
